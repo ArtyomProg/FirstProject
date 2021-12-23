fa79c3f4782aad7b8b9a2c5ac96c4639d1c9d746
ArtyomProg <malakyan.artyom@gmail.com>
2021-12-23 21:33:45 +0400

    updated names, updated .gitignore

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import say_hello, main_page
+from .views import say_hello2, main_page
 
 urlpatterns = [
-    path('', say_hello),
+    path('', say_hello2),
     path('main', main_page)
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 def say_hello(request):
 	return render(request, "index.html")
 
 def main_page(request):
-	return render(request, "main.html")	
+	return render(request, "main.html")
